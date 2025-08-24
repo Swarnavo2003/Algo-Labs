987889267a65d1958b0c515f933deb53c625f5df
Swarnavo2003 <swarnavo2020@gmail.com>
2025-08-25 00:34:35 +0530

    Frontend Started

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <h1 className="text-3xl font-semibold mr-2">Lets Build AlgoLabs</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
