691ef3c562a5d54a0e3ae0ff10469f6db4406dd9
Swarnavo2003 <swarnavo2020@gmail.com>
2025-09-04 22:19:16 +0530

    Index.js File Configured

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,39 @@
-import app from "./app.js";
 import dotenv from "dotenv";
-dotenv.config({path: ".env", quiet: true});
+dotenv.config({path: "./.env", quiet: true});
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import healthCheckRouter from "./routes/healthCheck.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import {errorHandler} from "./utils/error-handler.js";
+import problemRouter from "./routes/problem.routes.js";
+import executeCodeRouter from "./routes/executeCode.routes.js";
+import submissionRouter from "./routes/submission.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
 
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    exposedHeaders: ["set-cookie", "*"],
+  }),
+);
+
+app.use("/api/v1/health-check", healthCheckRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/problems", problemRouter);
+app.use("/api/v1/execute-code", executeCodeRouter);
+app.use("/api/v1/submission", submissionRouter);
+app.use("/api/v1/playlist", playlistRouter);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
