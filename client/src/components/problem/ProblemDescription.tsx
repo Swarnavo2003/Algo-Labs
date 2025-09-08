952f925f38363166f0ce263d956187a174dbdb58
Swarnavo2003 <swarnavo2020@gmail.com>
2025-09-08 18:33:16 +0530

    Problem Description Area Improved

--- client/src/components/problem/ProblemDescription.tsx
+++ client/src/components/problem/ProblemDescription.tsx
@@ -3,12 +3,20 @@ import { Badge } from "../ui/badge";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { ScrollArea } from "../ui/scroll-area";
 import type { Problem } from "@/types";
+import { useProblemStore } from "@/store/problem-store";
+import { CheckCircle } from "lucide-react";
 
 const ProblemDescription = ({ problem }: { problem: Problem }) => {
+  const { solvedProblems } = useProblemStore();
   const { allSubmissions } = useSubmissionStore();
   return (
     <ScrollArea className="h-[650px] p-2 pr-4">
-      <h1 className="text-3xl font-semibold">{problem.title}</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-semibold">{problem.title}</h1>
+        {problem && solvedProblems.some((p) => p.id === problem.id) && (
+          <CheckCircle className="ml-2 size-4 text-green-500" />
+        )}
+      </div>
       <p className="mt-2 text-sm text-gray-300">
         {allSubmissions.length} submissions
       </p>
