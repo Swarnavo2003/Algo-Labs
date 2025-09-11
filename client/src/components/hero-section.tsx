b1c0869a48347e89f80913eff933e66115a49da1
Swarnavo2003 <swarnavo2020@gmail.com>
2025-09-11 21:39:44 +0530

    Landing Page Image Updated

--- client/src/components/hero-section.tsx
+++ client/src/components/hero-section.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { TextEffect } from "@/components/ui/text-effect";
 import { AnimatedGroup } from "@/components/ui/animated-group";
 import { Link } from "react-router-dom";
+import AlgoLabsLanding from "../../public/algo-labs-landing-image.png";
 import Features from "./ui/features";
 
 const transitionVariants = {
@@ -183,7 +184,7 @@ export default function HeroSection() {
               <div className="inset-shadow-2xs ring-background dark:inset-shadow-white/20 bg-background relative mx-auto max-w-6xl overflow-hidden rounded-2xl border p-4 shadow-lg shadow-zinc-950/15 ring-1">
                 <img
                   className="bg-background aspect-15/8 relative hidden rounded-2xl dark:block"
-                  src="/mail2.png"
+                  src={AlgoLabsLanding}
                   alt="app screen"
                   width="2700"
                   height="1440"
